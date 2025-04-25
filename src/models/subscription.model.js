a14f404115602d9ba21b32fca9cfdfbc3d328a45
Sanket Kumar <kumarsanket520@gmail.com>
2025-04-25 18:18:35 +0530

    routes and changes

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,13 +1,13 @@
 import mongoose, {Schema} from "mongoose"
-import { User } from "./user.model"
+import { User } from "./user.model.js"
 
 const subscriptionSchema = new Schema({
     subscriber:{
-        type : Schema.Type.ObjectId, //one whom is subscribing
+        type : Schema.Types.ObjectId, //one whom is subscribing
         ref:User
     },
     channel:{
-        type : Schema.Type.ObjectId, //one to whom 'subscriber'is subscribing
+        type : Schema.Types.ObjectId, //one to whom 'subscriber'is subscribing
         ref : "User"
     }
 },{timestamps:true})
